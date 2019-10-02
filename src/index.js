9bac96e1edcd95d95fd38ea771ae1f184b9e4b67
Cristina Dobos <dobos.cris@yahoo.com>
2019-10-02 13:18:08 +0300

    Test Gratian M

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import "./styles.css";
+import axios from "axios";
+document.getElementById("myName").innerHTML = "Hello, Gratian Muntean!";
 
-fetch('https://5d93a214e020b300147dafad.mockapi.io/candies')
-.then(response => response.json())
-.then(data => {
-  console.log(data) // Prints result from `response.json()` in getRequest
-})
-.catch(error => console.error(error))
+const list = document.getElementById("unordered");
+const resp = axios
+  .get("https://5d93a214e020b300147dafad.mockapi.io/candies")
+  .then(res => {
+    console.log(res);
+    console.log(res.data);
+    if (res.data) {
+      res.data.map((el, i) => {
+        const element = document.createElement("LI");
+        const text = document.createTextNode(el.brand);
+        element.appendChild(text);
+        list.appendChild(element);
+      });
+    }
+
+    return res.json();
+  });
